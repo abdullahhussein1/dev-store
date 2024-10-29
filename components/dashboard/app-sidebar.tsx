dcf933fa8fc2143b9f7e51b7a7c175b04f159832
Abdullah Hussein <abdullabale11@gmail.com>
2024-10-29 21:59:17 +0300

    refactor: move dashboard files to its folders

---
+++ components/dashboard/app-sidebar.tsx
@@ -0,0 +1,173 @@
+"use client";
+
+import * as React from "react";
+import {
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  Command,
+  Home,
+  Settings,
+  Settings2,
+  ShoppingCart,
+  SquareTerminal,
+  Terminal,
+} from "lucide-react";
+
+import { NavFilters } from "@/components/dashboard/nav-filters";
+import { NavMain } from "@/components/dashboard/nav-main";
+import { NavUser } from "@/components/dashboard/nav-user";
+import { TeamSwitcher } from "@/components/dashboard/team-switcher";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+
+const data = {
+  user: {
+    name: "Abdullah",
+    email: "abdullah@moon.dev",
+    avatar: "/avatars/profile.jpeg",
+  },
+  teams: [
+    {
+      name: "Dev Store",
+      logo: Terminal,
+      plan: "Enterprise",
+    },
+    {
+      name: "Acme Corp.",
+      logo: AudioWaveform,
+      plan: "Startup",
+    },
+    {
+      name: "Evil Corp.",
+      logo: Command,
+      plan: "Free",
+    },
+  ],
+  main: [
+    {
+      name: "Home",
+      url: "/dashboard",
+      icon: Home,
+    },
+    {
+      name: "Shopping Cart",
+      url: "/shopping-cart",
+      icon: ShoppingCart,
+    },
+    {
+      name: "Settings",
+      url: "/settings",
+      icon: Settings,
+    },
+  ],
+  filters: [
+    {
+      title: "Playground",
+      url: "#",
+      icon: SquareTerminal,
+      items: [
+        {
+          title: "History",
+          url: "#",
+        },
+        {
+          title: "Starred",
+          url: "#",
+        },
+        {
+          title: "Settings",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Models",
+      url: "#",
+      icon: Bot,
+      items: [
+        {
+          title: "Genesis",
+          url: "#",
+        },
+        {
+          title: "Explorer",
+          url: "#",
+        },
+        {
+          title: "Quantum",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Documentation",
+      url: "#",
+      icon: BookOpen,
+      items: [
+        {
+          title: "Introduction",
+          url: "#",
+        },
+        {
+          title: "Get Started",
+          url: "#",
+        },
+        {
+          title: "Tutorials",
+          url: "#",
+        },
+        {
+          title: "Changelog",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Settings",
+      url: "#",
+      icon: Settings2,
+      items: [
+        {
+          title: "General",
+          url: "#",
+        },
+        {
+          title: "Team",
+          url: "#",
+        },
+        {
+          title: "Billing",
+          url: "#",
+        },
+        {
+          title: "Limits",
+          url: "#",
+        },
+      ],
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.main} />
+        <NavFilters items={data.filters} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
---
+++ components/dashboard/nav-main.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import {
+  type LucideIcon,
+} from "lucide-react"
+
+import {
+  SidebarGroup,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { usePathname } from "next/navigation"
+import { cn } from "@/lib/utils"
+
+export function NavMain({
+  items,
+}: {
+  items: {
+    name: string
+    url: string
+    icon: LucideIcon
+  }[]
+}) {
+  const pathname = usePathname();
+
+  return (
+    <SidebarGroup>
+      <SidebarMenu>
+        {items.map((item) => (
+          <SidebarMenuItem key={item.name}>
+            <SidebarMenuButton asChild className={cn(pathname === item.url && "bg-sidebar-accent")}>
+              <a href={item.url}>
+                <item.icon />
+                <span>{item.name}</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        ))}
+      </SidebarMenu>
+    </SidebarGroup>
+  )
+}
