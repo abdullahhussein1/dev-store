16e041035232f3cf0454d1894d5380a5d3037b7f
Abdullah Hussein <abdullabale11@gmail.com>
2024-11-01 21:49:42 +0300

    add shadcn to marquee

--- components/home/main/stack-marquee.tsx
+++ components/home/main/stack-marquee.tsx
@@ -30,49 +30,49 @@ export function StackMarquee({
       {
         src: "/stacks/nextjs-dark.svg",
         alt: "Next.js Logo",
-        className: "hidden w-24 px-2 dark:block md:w-28",
+        className: "hidden w-24 pe-4 dark:block md:w-28",
       },
       {
         src: "/stacks/supabase-dark.svg",
         alt: "Supabase Logo",
-        className: "hidden w-32 px-2 dark:block md:w-36",
+        className: "hidden w-32 pe-4 dark:block md:w-36",
       },
       {
         src: "/stacks/vercel-light.svg",
         alt: "Vercel Logo",
-        className: "hidden w-24 px-2 dark:block md:w-28",
+        className: "hidden w-24 pe-4 dark:block md:w-28",
       },
       {
         src: "/stacks/framer-light.svg",
         alt: "Framer Logo",
-        className: "hidden w-24 px-2 dark:block md:w-28",
+        className: "hidden w-24 pe-4 dark:block md:w-28",
       },
       {
         src: "/stacks/nextjs-light.svg",
         alt: "Next.js Logo",
-        className: "block w-24 px-2 dark:hidden md:w-28",
+        className: "block w-24 pe-4 dark:hidden md:w-28",
       },
       {
         src: "/stacks/supabase-light.svg",
         alt: "Supabase Logo",
-        className: "block w-32 px-2 dark:hidden md:w-36",
+        className: "block w-32 pe-4 dark:hidden md:w-36",
       },
       {
         src: "/stacks/vercel-dark.svg",
         alt: "Vercel Logo",
-        className: "block w-24 px-2 dark:hidden md:w-28",
+        className: "block w-24 pe-4 dark:hidden md:w-28",
       },
       {
         src: "/stacks/framer-dark.svg",
         alt: "Framer Logo",
-        className: "block w-24 px-2 dark:hidden md:w-28",
+        className: "block w-24 pe-4 dark:hidden md:w-28",
       },
     ],
     [],
   );
 
   return (
-    <div className="relative flex max-w-sm flex-col items-center justify-center gap-8 overflow-hidden py-12 md:max-w-2xl md:gap-12 lg:max-w-none">
+    <div className="relative flex max-w-sm flex-col items-center justify-center gap-8 overflow-hidden py-12 md:max-w-screen-sm md:gap-12 lg:max-w-screen-lg">
       <BlurFade className="z-10 text-3xl font-bold md:text-5xl">
         {title}
       </BlurFade>
@@ -80,6 +80,7 @@ export function StackMarquee({
         {stackImages.map((image, index) => (
           <StackImage key={index} {...image} />
         ))}
+        <div className="flex items-center gap-1 pe-4"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="h-6 w-6 md:size-8"><rect width="256" height="256" fill="none"></rect><line x1="208" y1="128" x2="128" y2="208" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"></line><line x1="192" y1="40" x2="40" y2="192" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"></line></svg><p className="font-bold md:text-xl">shadcn/ui</p></div>
       </Marquee>
       <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-secondary dark:from-background" />
       <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-secondary dark:from-background" />
