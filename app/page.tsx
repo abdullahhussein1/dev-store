8634db18893ae7a7b453d58a1c27168c558bd990
Abdullah Hussein <abdullabale11@gmail.com>
2024-11-01 17:07:20 +0300

    update marquee

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,9 @@ export default function Home() {
       <div className="fixed top-0 z-50 h-16 w-full rounded-b-[40px] bg-background/10 backdrop-blur-sm"></div>
       <div className="absolute -left-12 -top-8 hidden size-1/3 rounded-full bg-background blur-3xl dark:block dark:bg-secondary/40"></div>
       <Navbar />
-      <main className="mx-auto flex max-w-screen-lg flex-col items-center justify-center gap-48 p-8 pt-12 md:gap-32 md:pt-20">
+      <main className="flex max-w-screen-lg flex-col items-center justify-center gap-48 overflow-x-clip p-8 pt-12 md:mx-auto md:gap-32 md:pt-20">
         <HeroSection />
-        <section className="flex flex-col">
-          <StackMarquee />
-        </section>
+        <StackMarquee />
       </main>
     </div>
   );
--- components/home/main/stack-marquee.tsx
+++ components/home/main/stack-marquee.tsx
@@ -4,36 +4,56 @@ import Image from "next/image";
 
 export function StackMarquee() {
   return (
-    <div className="relative flex w-full flex-col items-center justify-center gap-12 bg-gradient-to-b from-background to-transparent py-12 md:shadow-xl">
-      <BlurFade className="z-10 text-3xl font-semibold lg:text-5xl" inView>
-        Used Cutting Edge Stacks
+    <div className="relative flex flex-col items-center justify-center gap-8 to-background py-12 md:gap-12">
+      <BlurFade className="z-10 text-3xl font-semibold md:text-5xl">
+        Build With
       </BlurFade>
-      <Marquee reverse className="[--duration:20s]">
+      <Marquee className="[--duration:20s]">
         <Image
           src="/stacks/nextjs-dark.svg"
           alt="stack image"
-          className="w-24 px-2 md:w-44"
-          height="80"
-          width="100"
+          className="hidden w-24 px-2 dark:block md:w-28"
+          height="10"
+          width="10"
         />
         <Image
           src="/stacks/supabase-dark.svg"
           alt="stack image"
-          className="w-32 px-2 md:w-44"
-          height="80"
-          width="100"
+          className="hidden w-32 px-2 dark:block md:w-36"
+          height="10"
+          width="10"
         />
-
         <Image
           src="/stacks/vercel-light.svg"
           alt="stack image"
-          className="w-24 px-2 md:w-44"
-          height="256"
-          width="256"
+          className="hidden w-24 px-2 dark:block md:w-28"
+          height="10"
+          width="10"
+        />
+        <Image
+          src="/stacks/nextjs-light.svg"
+          alt="stack image"
+          className="w-24 px-2 dark:hidden md:w-28"
+          height="10"
+          width="10"
+        />
+        <Image
+          src="/stacks/supabase-light.svg"
+          alt="stack image"
+          className="w-32 px-2 dark:hidden md:w-36"
+          height="10"
+          width="10"
+        />
+        <Image
+          src="/stacks/vercel-dark.svg"
+          alt="stack image"
+          className="w-24 px-2 dark:hidden md:w-28"
+          height="10"
+          width="10"
         />
       </Marquee>
-      <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
-      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
+      <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-secondary dark:from-background"></div>
+      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-secondary dark:from-background"></div>
     </div>
   );
 }
