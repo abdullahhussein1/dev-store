32a8ae3100fe1e0a8fc01a7bff00771da7a69935
Abdullah Hussein <abdullabale11@gmail.com>
2024-11-06 21:37:55 +0300

    minor update

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Navbar from "@/components/home/navbar/navbar";
 export default function Home() {
   return (
     <div className="relative flex min-h-screen flex-col items-center justify-items-center overflow-x-clip bg-secondary p-4 font-[family-name:var(--font-geist-sans)] dark:bg-background">
-      <div className="fixed top-0 z-50 h-16 w-full rounded-b-[40px] bg-background/10 backdrop-blur-sm"></div>
+      <div className="fixed top-0 z-50 mx-auto h-16 w-full max-w-screen-lg rounded-b-[40px] bg-background/10 backdrop-blur-sm"></div>
       <div className="absolute -left-12 -top-8 hidden size-1/3 rounded-full bg-background blur-3xl dark:block dark:bg-secondary/40" />
       <Navbar />
       <main className="flex flex-col items-center justify-center gap-44 p-4 pt-12 md:mx-auto md:max-w-screen-lg md:gap-64 md:p-12 md:pt-20">
