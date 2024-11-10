5bb954d44158e550e9b9faf2770f5425c4346684
Abdullah Hussein <abdullabale11@gmail.com>
2024-11-10 20:36:57 +0300

    update

--- components/home/footer.tsx
+++ components/home/footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
         <Terminal strokeWidth={3} className="size-5 rounded-full border p-1" />
         <p className="font-medium text-foreground/70">© 2024</p>
       </div>
-      <div className="flex gap-1 font-medium text-foreground/70">
+      <div className="flex gap-1 text-[11px] font-medium text-foreground/70">
         <p>Stands with</p>
         <p className="font-semibold text-foreground/90 transition-all hover:text-foreground">
           Palestine 🇵🇸 Sudan 🇸🇩 Syria 🇸🇾
