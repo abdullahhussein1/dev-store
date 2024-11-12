e1ad53fe91c4d24c4c6222ad915e45f71a791b9b
Abdullah Hussein <abdullabale11@gmail.com>
2024-11-12 17:07:19 +0300

    some updates

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,15 @@
 import { Button } from "@/components/ui/button";
-import { Heart, Star } from "lucide-react";
+import {
+  Battery,
+  Cpu,
+  Heart,
+  LucideIcon,
+  Maximize2,
+  MemoryStick,
+  Star,
+} from "lucide-react";
 import { Metadata } from "next";
 import Image from "next/image";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Separator } from "@/components/ui/separator";
 
@@ -24,23 +24,10 @@ export default function Home() {
         <div className="flex items-center gap-2 px-4">
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
         </div>
       </header>
       <main className="flex flex-1 flex-col gap-6 p-4 pt-0">
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+        <div className="grid-cols-auto-fit-300 grid gap-4">
           <ItemCart />
           <ItemCart />
           <ItemCart />
@@ -57,8 +44,8 @@ export default function Home() {
 
 function ItemCart() {
   return (
-    <div className="group relative flex cursor-pointer justify-center gap-4 rounded-3xl bg-secondary/40 p-2">
-      <div className="flex aspect-square w-1/3 items-center justify-center overflow-clip rounded-3xl border border-secondary/60 bg-white">
+    <div className="group relative flex cursor-pointer justify-center gap-2 rounded-3xl bg-secondary/50 p-2">
+      <div className="flex aspect-square w-1/3 items-center justify-center overflow-clip rounded-3xl border border-secondary/70 bg-white">
         <Image
           src="/macbook.jpg"
           width={1000}
@@ -74,7 +61,7 @@ function ItemCart() {
       >
         <Heart />
       </Button>
-      <div className="flex flex-1 flex-col gap-1 p-2 text-foreground/90 transition-all hover:text-foreground">
+      <div className="flex flex-1 flex-col gap-1 p-2 text-foreground/90 transition-all group-hover:text-foreground">
         <p className="text-lg font-semibold">Macbook M3 2024</p>
         <div className="flex items-center gap-1 text-foreground/30 [&_svg]:size-4">
           <Star className="fill-foreground/20" />
@@ -87,7 +74,51 @@ function ItemCart() {
           <p className="text-lg font-semibold">$899.99</p>
           <p className="text-sm text-foreground/70">5 left in Stack</p>
         </div>
+        <div className="grid flex-1 grid-cols-2 place-content-start py-1 text-sm font-semibold text-foreground/50 [&_svg]:size-4">
+          <FeatureItem Icon={Cpu} title="CPU" value="M3" />
+          <FeatureItem Icon={Maximize2} title="Display" value="14'" />
+          <FeatureItem Icon={MemoryStick} title="RAM" value="8" />
+          <FeatureItem Icon={Battery} title="Battery" value="3600" />
+        </div>
       </div>
     </div>
+  );
+}
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+function FeatureItem({
+  Icon,
+  title,
+  value,
+}: {
+  Icon: LucideIcon;
+  title: string;
+  value: string;
+}) {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger asChild>
+          <Button
+            variant="ghost"
+            size="sm"
+            className="flex w-fit items-center gap-2"
+          >
+            <Icon /> <p>{value}</p>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>
+            {title}: {value}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
